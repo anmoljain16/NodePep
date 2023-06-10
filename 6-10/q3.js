2098e68a062fa302de7f4ad9cb6500860760fee0
anmoljain16 <75519593+anmoljain16@users.noreply.github.com>
2023-06-10 09:35:53 +0530

    Update q3.js

--- 6-10/q3.js
+++ 6-10/q3.js
@@ -4,13 +4,8 @@ const l = input.length;
 
 var total = 0;
 
-input.filter( (num) => {
-        return num >0;
-    }
-
-).map( (num) => {
+input.map( (num) => {
     total = total +num;
 } )
 
 console.log(`Mean = ${total/l}`);
-
